19043520fc6b28fd548ea57be43731db8595be35
Ed on ubuntu1 <ed@bestpathresearch.com>
2024-09-03 20:19:45 +0900

    edw: Added an optional offset argument to TextDecoder:forward().

--- whisper/model.py
+++ whisper/model.py
@@ -193,30 +193,50 @@ class TextDecoder(nn.Module):
         mask = torch.empty(n_ctx, n_ctx).fill_(-np.inf).triu_(1)
         self.register_buffer("mask", mask, persistent=False)
 
-    def forward(self, x: Tensor, xa: Tensor, kv_cache: Optional[dict] = None):
+    def forward(self, x: Tensor, xa: Tensor, kv_cache: Optional[dict] = None, offset: Optional[Int] = 0):
         """
         x : torch.LongTensor, shape = (batch_size, <= n_ctx)
             the text tokens
         xa : torch.Tensor, shape = (batch_size, n_audio_ctx, n_audio_state)
             the encoded audio features to be attended on
         """
-        offset = next(iter(kv_cache.values())).shape[1] if kv_cache else 0
-        x = (
-            self.token_embedding(x)
-            + self.positional_embedding[offset : offset + x.shape[-1]]
-        )
-        x = x.to(xa.dtype)
 
-        for block in self.blocks:
-            x = block(x, xa, mask=self.mask, kv_cache=kv_cache)
+        if True:
+            offset = next(iter(kv_cache.values())).shape[1] if kv_cache else 0
+            x = (
+                self.token_embedding(x)
+                + self.positional_embedding[offset : offset + x.shape[-1]]
+            )
+            x = x.to(xa.dtype)
+            
+            for block in self.blocks:
+                x = block(x, xa, mask=self.mask, kv_cache=kv_cache)
+                
+            x = self.ln(x)
+            logits = (
+                x @ torch.transpose(self.token_embedding.weight.to(x.dtype), 0, 1)
+            ).float()
 
-        x = self.ln(x)
-        logits = (
-            x @ torch.transpose(self.token_embedding.weight.to(x.dtype), 0, 1)
-        ).float()
+            return logits
 
-        return logits
+        if True:
+            #offset = next(iter(kv_cache.values())).shape[1] if kv_cache else 0
+            x = (
+                self.token_embedding(x)
+                + self.positional_embedding[offset : offset + x.shape[-1]]
+            )
+            x = x.to(xa.dtype)
+            
+            for block in self.blocks:
+                x = block(x, xa, mask=self.mask, kv_cache=kv_cache)
+                
+            x = self.ln(x)
+            logits = (
+                x @ torch.transpose(self.token_embedding.weight.to(x.dtype), 0, 1)
+            ).float()
 
+            return logits
+            
 
 class Whisper(nn.Module):
     def __init__(self, dims: ModelDimensions):
